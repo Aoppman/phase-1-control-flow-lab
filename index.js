e6ce180b69e2fc6fd66b8d2f456ac570aa7d26a7
Andrew <a.oppman@gmail.com>
2023-06-27 18:18:38 -0400

    update to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let distance = 199
+/*let distance = 199
 
 
 function scuberGreetingForFeet(distance){
@@ -40,4 +40,39 @@ function switchOnCharmFromTip(tip){
     default: "if anything else";
     return ("Bye.");
   }
-}
+}*/
+
+
+//let distance = 199
+
+function scuberGreetingForFeet(distance){
+  if (distance > 2500) {
+    return 'No can do.';
+  }
+  else if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance > 400 && distance < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else {
+    return 'I will gladly take your thirty bucks.'
+  }
+  return result
+};
+
+function ternaryCheckCity(city) {
+ return (city === 'NYC' ? ('Ok, sounds good.') : ('No go.'));
+};
+
+
+const switchOnCharmFromTip = (generosity) => {
+  switch (generosity) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: 
+      return 'Bye.';
+  };
+};
